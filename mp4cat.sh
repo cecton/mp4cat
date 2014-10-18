5fa2146fd2fb98f3795059e22f1c64e283ff4585
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-18 16:09:16 +0200

    Fixed: do no create tmp dir if arguments are bad (and clean up)

--- mp4cat.sh
+++ mp4cat.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-tmp=`mktemp -d`
+[ ${#@} -lt 3 ] && echo "too few arguments" && exit 1
+ls "${@:1:$((${#@}-1))}" > /dev/null || exit 1
+[ -f "${@:$#}" ] && echo "${@:$#}: file exists" && exit 1
 
-[ ${#@} -lt 2 ] && echo "too few arguments" && exit 1
-[ -f "${@:$#:1}" ] && echo "file exists" && exit 1
+tmp=`mktemp -d`
 
 i=0
 concat=
-for file in "$@"
+for input in "${@:1:$((${#@}-1))}"
 do
-	if [ $((++i)) -lt ${#@} ]; then
-		concat="$concat|$tmp/$i"
-		mkfifo "$tmp/$i"
-		ffmpeg -y -i "$file" -c copy -bsf:v h264_mp4toannexb -f mpegts "$tmp/$i" 2> "$tmp/stderr" &
-	else
-		ffmpeg -i "concat:${concat:1}" -c copy -bsf:a aac_adtstoasc "$file"
-	fi
+	concat="$concat|$tmp/"$((++i))
+	mkfifo "$tmp/$i"
+	ffmpeg -y -i "$input" -c copy -bsf:v h264_mp4toannexb \
+		-nostats -f mpegts "$tmp/$i" 2>> "$tmp/stderr" &
 done
+
+ffmpeg -i "concat:${concat:1}" -c copy -bsf:a aac_adtstoasc "${@:$#}"
+res=$?
 
 wait
 rm -f "$tmp"/*
 rmdir "$tmp"
+
+exit $res
